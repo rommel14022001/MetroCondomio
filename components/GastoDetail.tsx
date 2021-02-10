eaa129e0b14ebbfe078e7790af3f0e5cdfec8fff
rommel sanzonetti <rommel.sanzonetti@correo.unimet.edu.ve>
2021-02-10 13:21:35 -0400

    syncdb lista

--- components/GastoDetail.tsx
+++ components/GastoDetail.tsx
@@ -4,11 +4,12 @@ import styles from '../styles/components/gastosDetails.module.css';
 import {Col} from 'react-bootstrap'
 export const GastosDetails  = ({gasto, deleteGasto}) => {
     
-    const {name,amount} = gasto;
+    const {nombre,monto} = gasto;
 
     return(
-        <Col xs={5} lg={5} md={5} className = {styles.gastosDetails}>
-            <div> {name} </div>
+        <Col xs={10} lg={10} md={10} className = {styles.gastosDetails}>
+            <Col> {nombre} </Col>
+            <Col>${monto}</Col>
             <button className="btn-outline btn-danger" onClick = {() => deleteGasto(name)}>Eliminar</button>
         </Col>
     )
