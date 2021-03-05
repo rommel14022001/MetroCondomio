828c39641592ae3584554e85d0c8333c2619c209
gebrayel <gebrayel.inatti@correo.unimet.edu.ve>
2021-03-04 21:24:37 -0400

    sth

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,8 +1,12 @@
-import React, { useContext, useLayoutEffect, useState } from 'react'
+import React, { useContext, useLayoutEffect, useState, useEffect } from 'react'
 import {Nav} from "react-bootstrap";
 
 export const Sidebar = () => {
 
+    useEffect(() => {
+        console.log("data")
+        
+    }, [])
     return(
         <Nav className="col-md-12 d-none d-md-block bg-light sidebar">
             <div className="sidebar-sticky"></div>
@@ -10,7 +14,12 @@ export const Sidebar = () => {
             <Nav.Item>
                 <Nav.Link className="navlink">Registrar Pagos</Nav.Link>
             </Nav.Item>
-            
+            <Nav.Item>
+                <Nav.Link className="navlink">Administrar Condo</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link className="navlink" >Administrar Gastos</Nav.Link>
+            </Nav.Item>
         </Nav>
     )
 }
